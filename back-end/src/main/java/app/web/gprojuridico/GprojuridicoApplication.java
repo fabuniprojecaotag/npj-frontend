db9dd6ec0116fb0600cc7edb314e56a4305ab6ba
Igor <igbit@outlook.com>
2023-11-05 20:58:26 -0300

    Feat(Swagger-ui)

--- back-end/src/main/java/app/web/gprojuridico/GprojuridicoApplication.java
+++ back-end/src/main/java/app/web/gprojuridico/GprojuridicoApplication.java
@@ -2,12 +2,22 @@ package app.web.gprojuridico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZonedDateTime;
 
 @SpringBootApplication
+@RestController
 public class GprojuridicoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GprojuridicoApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello(){
+		return "Service is On, Started at "+ ZonedDateTime.now();
 	}
 
 }
