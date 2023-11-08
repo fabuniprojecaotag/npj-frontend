3e751344ba2369bceb47b80b275abb837f9d9bff
Igor <igbit@outlook.com>
2023-11-08 09:42:06 -0300

    Model user

---
+++ back-end/src/main/java/app/web/gprojuridico/Models/User.java
@@ -0,0 +1,79 @@
+package app.web.gprojuridico.Models;
+
+import org.apache.naming.factory.SendMailFactory;
+
+
+public class User {
+
+    private int id;
+    private String email;
+    private String senha;
+
+    private String matricula;
+    private String nome;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    private String token;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getPerfil_id() {
+        return perfil_id;
+    }
+
+    public void setPerfil_id(int perfil_id) {
+        this.perfil_id = perfil_id;
+    }
+
+    private int  perfil_id;
+
+    User(){
+
+    }
+
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
