1a62f7981fad91ac7ebe64cecfd511619f07629e
Igor <igbit@outlook.com>
2023-11-08 09:49:23 -0300

    Adicionado Inicialização da conexão do firestore

--- back-end/src/main/java/app/web/gprojuridico/GprojuridicoApplication.java
+++ back-end/src/main/java/app/web/gprojuridico/GprojuridicoApplication.java
@@ -1,23 +1,17 @@
 package app.web.gprojuridico;
 
+import app.web.gprojuridico.Firebase.FirebaseConnection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.ZonedDateTime;
 
 @SpringBootApplication
-@RestController
 public class GprojuridicoApplication {
 
 	public static void main(String[] args) {
+
+		FirebaseConnection.initialization();
 		SpringApplication.run(GprojuridicoApplication.class, args);
 	}
 
-	@GetMapping
-	public String hello(){
-		return "Service is On, Started at "+ ZonedDateTime.now();
-	}
 
 }
