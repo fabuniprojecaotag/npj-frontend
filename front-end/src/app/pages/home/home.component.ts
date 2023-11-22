0a2a064d39011c6276c55341205629c225d2e803
Rafael Aguiar Rodrigues <rafaelrodriguesaguiar18@gmail.com>
2023-11-22 15:46:53 -0300

    alterando design do header

--- front-end/src/app/pages/home/home.component.ts
+++ front-end/src/app/pages/home/home.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  tituloDaPagina: string = 'home';
+  tituloDaPagina: string = 'Home';
 }
