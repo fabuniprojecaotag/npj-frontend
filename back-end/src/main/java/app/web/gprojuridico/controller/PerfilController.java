bb48285ccdaeeb33f35b4213cab0eb134616c389
Igor <igbit@outlook.com>
2023-11-27 22:15:31 -0300

    Contoller-Perfil(Novos metodos adicionados)

---
+++ back-end/src/main/java/app/web/gprojuridico/controller/PerfilController.java
@@ -0,0 +1,41 @@
+package app.web.gprojuridico.controller;
+
+
+import app.web.gprojuridico.model.Perfil;
+import app.web.gprojuridico.model.ResponseModel;
+import app.web.gprojuridico.model.User;
+import app.web.gprojuridico.service.PerfilService;
+import app.web.gprojuridico.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping("/perfil")
+public class PerfilController {
+
+    @Autowired
+    private final PerfilService perfilService;
+
+    public PerfilController(PerfilService perfilService) {
+        this.perfilService = perfilService;
+    }
+    @GetMapping("/all")
+    public ResponseModel getAllUsers() {
+        ResponseModel data = perfilService.getAll();
+        return data;
+    }
+    @GetMapping("/{perfilId}")
+    public ResponseEntity<ResponseModel> getPerfilById(@PathVariable String perfilId) {
+        ResponseEntity<ResponseModel> perfilResponse = perfilService.getPerfilById(perfilId);
+
+        if (perfilResponse.getBody() != null) {
+            return new ResponseEntity<>(perfilResponse.getBody(), perfilResponse.getStatusCode());
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
