2c1c8331f955ba8702df274cb9df33614c7ac858
Igor <igbit@outlook.com>
2024-05-05 20:43:47 -0300

    Dialog de boas vindas ao sistema

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { MatDialog,MatDialogModule } from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   subtituloDaPagina: string = 'Home';
+  tituloDaPagina: string = 'Home';
+
+
+    constructor(public dialog:MatDialog) { 
+
+    }
+   
+    verifyLocalStorageDialogFlag(): any {
+      const item = localStorage.getItem(`firstTime`);
+      return item ? JSON.parse(item) : null;
+    }
+  
+    ngOnInit(): void {
+      if(!this.verifyLocalStorageDialogFlag()){
+        this.openDialog()
+      }
+    }
+    openDialog() {
+      const dialogRef = this.dialog.open(DialogContentExampleDialog);
+  
+      dialogRef.afterClosed().subscribe(result => {
+        localStorage.setItem('firstTime', JSON.stringify(true));
+        console.log(`Dialog result: ${result}`);
+
+      });
+    }
 }
+@Component({
+  selector: 'welcome-dialog',
+  templateUrl: 'welcome-dialog.html',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule],
+})
+export class DialogContentExampleDialog {}
