8b04f0f581f0b9f965ae3cc8662ea60c9d14ec32
Rafael Aguiar Rodrigues <rafaelrodriguesaguiar18@gmail.com>
2024-05-21 16:55:50 -0300

    testes de utils bars e header

--- src/app/shared/utils-bar/utils-bar.component.spec.ts
+++ src/app/shared/utils-bar/utils-bar.component.spec.ts
@@ -1,24 +1,47 @@
+import { Location } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { UtilsBarComponent } from './utils-bar.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { UtilsBarComponent } from './utils-bar.component';
 
 describe(UtilsBarComponent.name, () => {
   let component: UtilsBarComponent;
   let fixture: ComponentFixture<UtilsBarComponent>;
+  let locationMock: jasmine.SpyObj<Location>;
 
   beforeEach(() => {
+    locationMock = jasmine.createSpyObj<Location>('Location', ['back', 'forward']);
+
     TestBed.configureTestingModule({
       imports: [MatIconModule, MatToolbarModule],
-      declarations: [UtilsBarComponent]
-    });
+      declarations: [UtilsBarComponent],
+      providers: [{ provide: Location, useValue: locationMock }]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(UtilsBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    spyOn(window.location, 'reload');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call location.forward() on avancar()', () => {
+    component.avancar();
+    expect(locationMock.forward).toHaveBeenCalled();
+  });
+
+  it('should call location.back() on voltar()', () => {
+    component.voltar();
+    expect(locationMock.back).toHaveBeenCalled();
+  });
+
+  it('should call window.location.reload() on recarregar()', () => {
+    component.recarregar();
+    expect(window.location.reload).toHaveBeenCalled();
   });
 });
