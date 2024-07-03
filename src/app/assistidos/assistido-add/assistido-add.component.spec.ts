607cbbccb03b79b5f1fb4e83f83e2b674295e5bc
Rafael Aguiar Rodrigues <rafaelrodriguesaguiar18@gmail.com>
2024-07-02 21:50:53 -0300

    refatoração nos testes do modulo de atendimento

--- src/app/assistidos/assistido-add/assistido-add.component.spec.ts
+++ src/app/assistidos/assistido-add/assistido-add.component.spec.ts
@@ -1,37 +1,139 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { of } from 'rxjs';
-import { AssistidoTrabalhista } from 'src/app/core/types/assistido';
+import { NgZone } from '@angular/core'; // Importe NgZone
+import { Assistido } from 'src/app/core/types/assistido';
 import { AssistidosService } from '../services/assistidos.service';
-
 import { AssistidoAddComponent } from './assistido-add.component';
+import { FormsService } from 'src/app/core/services/forms.service';
+import { ViacepService } from 'src/app/core/services/viacep.service';
+import { HeaderComponent } from 'src/app/shared/header/header.component';
+import { UtilsBarComponent } from 'src/app/shared/utils-bar/utils-bar.component';
+import { FormAssistidosComponent } from 'src/app/shared/form-assistidos/form-assistidos.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { NavMenuComponent } from 'src/app/shared/header/nav-menu/nav-menu.component';
+import { UserMenuComponent } from 'src/app/shared/header/user-menu/user-menu.component';
+import { NavItemComponent } from 'src/app/shared/header/nav-menu/nav-item/nav-item.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { ModalAssistidoComponent } from 'src/app/shared/modal-assistido/modal-assistido.component';
 
-describe('AssistidoAddComponent', () => {
+const mockAssistido: Assistido = {
+  cpf: '123.456.789-00',
+  nome: 'Fulaninho',
+  email: 'fulaninho@example.com',
+  rg: '1234567',
+  endereco: {
+    residencial: {
+      logradouro: 'Rua Principal',
+      bairro: 'Centro',
+      numero: '123',
+      complemento: 'Ap. 101',
+      cep: '12345-678',
+      cidade: 'Cidade Exemplo'
+    }
+  },
+  filiacao: {
+    pai: 'Fulano Pai',
+    mae: 'Fulano Mãe'
+  },
+};
+
+class MockAssistidosService {
+  cadastrarAssistido(assistido: Assistido) {
+    return of(mockAssistido);
+  }
+}
+
+const mockViacepResponse = {
+  cep: '12345678',
+  bairro: 'Bairro Teste',
+  localidade: 'Cidade Teste',
+  logradouro: 'Rua Teste',
+  complemento: 'Complemento Teste',
+  uf: '',
+  ibge: '',
+  gia: '',
+  ddd: '',
+  siafi: ''
+};
+
+class MockViacepService {
+  consultarCep(cep: string) {
+    return of(mockViacepResponse);
+  }
+}
+
+describe(AssistidoAddComponent.name, () => {
   let component: AssistidoAddComponent;
   let fixture: ComponentFixture<AssistidoAddComponent>;
-  let mockAssistidosService: jasmine.SpyObj<AssistidosService>;
+  let assistidosService: MockAssistidosService;
+  let viacepService: MockViacepService;
+  let dialog: MatDialog;
+  let formsService: FormsService;
+  let router: Router;
+  let ngZone: NgZone; 
 
   beforeEach(async () => {
-    mockAssistidosService = jasmine.createSpyObj('AssistidosService', ['cadastrarAssistido']);
-
     await TestBed.configureTestingModule({
-      declarations: [ AssistidoAddComponent ],
+      declarations: [
+        AssistidoAddComponent,
+        HeaderComponent,
+        UtilsBarComponent,
+        FormAssistidosComponent,
+        NavMenuComponent,
+        UserMenuComponent,
+        NavItemComponent
+      ],
       imports: [
+        BrowserAnimationsModule,
+        MatExpansionModule,
         MatDialogModule,
-        RouterTestingModule
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule,
+        MatSelectModule,
+        MatToolbarModule,
+        ReactiveFormsModule,
+        RouterTestingModule.withRoutes([
+          { path: 'assistidos/list', component: AssistidoAddComponent }
+        ]),
+        NgxMaskDirective,
+        NgxMaskPipe,
       ],
       providers: [
-        { provide: AssistidosService, useValue: mockAssistidosService },
-        { provide: MatDialogRef, useValue: {} }
+        { provide: AssistidosService, useClass: MockAssistidosService },
+        { provide: ViacepService, useClass: MockViacepService },
+        { provide: MatDialog, useValue: { open: () => {} } },
+        provideNgxMask()
       ]
-    })
-      .compileComponents();
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AssistidoAddComponent);
     component = fixture.componentInstance;
+    assistidosService = TestBed.inject(AssistidosService) as unknown as MockAssistidosService;
+    viacepService = TestBed.inject(ViacepService) as unknown as MockViacepService;
+    dialog = TestBed.inject(MatDialog);
+    formsService = TestBed.inject(FormsService);
+    router = TestBed.inject(Router);
+    ngZone = TestBed.inject(NgZone); // Injete NgZone
+
+    // Simular que o formulário é válido e retorna mockAssistido
+    spyOn(formsService, 'getForm').and.returnValue({
+      valid: true,
+      getRawValue: () => mockAssistido
+    } as any);
+
     fixture.detectChanges();
   });
 
@@ -39,44 +141,30 @@ describe('AssistidoAddComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should call cadastrar() on click', () => {
+  it('should call cadastrar() on button click', () => {
     spyOn(component, 'cadastrar');
-    const button = fixture.debugElement.nativeElement.querySelector('button');
-    button.click();
+    const button = fixture.debugElement.nativeElement.querySelector('button[type="submit"]');
+    button.dispatchEvent(new Event('click'));
+    fixture.detectChanges();
     expect(component.cadastrar).toHaveBeenCalled();
   });
 
-  it('should call AssistidosService.cadastrarAssistido() on cadastrar()', () => {
-    const mockAssistido: AssistidoTrabalhista = {
-      cpf: '123.456.789-00',
-      ctps: {
-        numero: '123456',
-        serie: '789',
-        uf: 'SP'
-      },
-      pis: '12345678900',
-      empregadoAtualmente: false,
-      nome: 'Fulaninho',
-      rg: '1234567',
-      endereco: {
-        residencial: {
-          logradouro: 'Rua Principal',
-          bairro: 'Centro',
-          numero: '123',
-          complemento: 'Ap. 101',
-          cep: '12345-678',
-          cidade: 'Cidade Exemplo'
-        }
-      },
-      filiacao: {
-        pai: 'Fulano Pai',
-        mae: 'Fulano Mãe'
-      },
-    };
-    mockAssistidosService.cadastrarAssistido.and.returnValue(of(mockAssistido));
+  it('should call cadastrarAssistido() with the correct data on successful cadastro', fakeAsync(() => {
+    spyOn(assistidosService, 'cadastrarAssistido').and.returnValue(of(mockAssistido));
+    spyOn(dialog, 'open').and.stub();
 
     component.cadastrar();
+    tick();
 
-    expect(mockAssistidosService.cadastrarAssistido).toHaveBeenCalled();
-  });
+    expect(assistidosService.cadastrarAssistido).toHaveBeenCalledWith(mockAssistido);
+    expect(dialog.open).toHaveBeenCalledWith(ModalAssistidoComponent, {
+      width: '552px',
+      height: '360px',
+      data: { operacao: 'criado', nome: mockAssistido.nome, email: mockAssistido.email, cpf: mockAssistido.cpf }
+    });
+
+    ngZone.run(() => {
+      expect(router.url).toBe('/assistidos/list');
+    });
+  }));
 });
