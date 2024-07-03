4fe24569d82036484c496c91b16119b37171e08e
Rafael Aguiar Rodrigues <rafaelrodriguesaguiar18@gmail.com>
2024-07-03 16:46:28 -0300

    testes da guard de alteração

--- src/app/core/guards/pending-changes.guard.spec.ts
+++ src/app/core/guards/pending-changes.guard.spec.ts
@@ -1,17 +1,57 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
-
+import { Router } from '@angular/router';
 import { pendingChangesGuard } from './pending-changes.guard';
 
-describe('pendingChangesGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => pendingChangesGuard(...guardParameters));
+describe(pendingChangesGuard.name, () => {
+  let guard: any;
+  let routerMock: jasmine.SpyObj<Router>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    routerMock = jasmine.createSpyObj('Router', ['navigate']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: Router, useValue: routerMock }
+      ]
+    });
+
+    guard = pendingChangesGuard;
   });
 
-  it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+  it('should allow navigation if there are no unsaved changes', () => {
+    const component: any = {
+      hasUnsavedChanges: () => false
+    };
+
+    const result = guard(component);
+
+    expect(result).toBe(true);
+    expect(routerMock.navigate).not.toHaveBeenCalled();
+  });
+
+  it('should prevent navigation and confirm if there are unsaved changes', () => {
+    const component: any = {
+      hasUnsavedChanges: () => true
+    };
+
+    spyOn(window, 'confirm').and.returnValue(true); // Simulando confirmação do usuário
+
+    const result = guard(component);
+
+    expect(result).toBe(true); // Deve permitir a navegação após confirmação do usuário
+    expect(routerMock.navigate).not.toHaveBeenCalled();
+  });
+
+  it('should prevent navigation if there are unsaved changes and user cancels', () => {
+    const component: any = {
+      hasUnsavedChanges: () => true
+    };
+
+    spyOn(window, 'confirm').and.returnValue(false); // Simulando cancelamento do usuário
+
+    const result = guard(component);
+
+    expect(result).toBe(false); // Deve impedir a navegação se o usuário cancelar
+    expect(routerMock.navigate).not.toHaveBeenCalled();
   });
 });
