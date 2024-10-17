73ba69b6ea38aa122f5f00404d9f36e4711d2a02
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2024-10-16 21:28:54 -0300

    Fix: Correção da Url, Feat: Implementar serviço para listar processos vinculados ao assistido

--- src/app/feature/processos/services/processos.service.ts
+++ src/app/feature/processos/services/processos.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
 import { map, Observable } from 'rxjs';
@@ -7,18 +7,27 @@ import { PaginationService } from 'src/app/core/services/pagination.service';
 import { Filtro } from 'src/app/core/types/filtro';
 import { ListCacheEntry } from 'src/app/core/types/list-cache-entry';
 import { Processo } from '../../../core/types/processo';
+import { ListResponse } from 'src/app/core/types/ListResponse';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProcessosService extends GenericService<Processo> {
-  private url = this.API + '/processos';
+  private url = this.API + '/assistidos';// O erro estava aqui...
   filter = { field: '', operator: '', value: '' };
 
   constructor(
     protected override http: HttpClient,
     protected override paginationService: PaginationService
   ) {
-    super(http, 'processos', paginationService)
+    super(http, 'processos', paginationService);
   }
+
+  // método para listar os processos vinculados
+  listarProcessos(id: string): Observable<ListResponse<Processo>>{
+    let params = new HttpParams().set('returnType', 'forAssistido');
+    return this.http.get<ListResponse<Processo>>(`${this.url}/${id}/processos`, {params});
+  }
+
 }
